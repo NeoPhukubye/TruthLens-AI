fb10701e17c16a124d3d838486bf8e1c2dc2ff43
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2026-07-20 08:17:24 +0200

    Initiate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { I18nProvider } from './hooks/useI18n'
+import { AuthProvider } from './hooks/useAuth'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <I18nProvider>
-      <BrowserRouter basename="/TruthLens-AI">
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter basename="/TruthLens-AI">
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </I18nProvider>
   </React.StrictMode>,
 )
